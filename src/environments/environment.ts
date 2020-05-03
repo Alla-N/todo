a91b7cdbeb2d83d255df3f711e3072549ad1bd52
Alla-N <Allasandra1725@gmail.com>
2020-05-03 19:44:26 +0300

    save data for deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,18 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyA4qMOK9bHcseL7g_tEi7MeWzyL5mzChT8',
-    authDomain: 'todo-9ff3d.firebaseapp.com',
-    databaseURL: 'https://todo-9ff3d.firebaseio.com',
-    projectId: 'todo-9ff3d',
-    storageBucket: 'todo-9ff3d.appspot.com',
-    messagingSenderId: '772491432248',
-    appId: '1:772491432248:web:1daeb5680e430afa55f1cd',
-    measurementId: 'G-FZS292QFR2'
-
-  }
+  production: false
 };
 
 /*
