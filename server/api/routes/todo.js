f70b2a974b056a90cb170db8bb1e84782c44ee91
Alla-N <Allasandra1725@gmail.com>
2020-05-17 17:52:05 +0300

    fix server and height aside

--- server/api/routes/todo.js
+++ server/api/routes/todo.js
@@ -3,106 +3,102 @@ const Router = express.Router;
 const router = new Router;
 const Todo = require('../models/Todo');
 
-router.get('/todo', async (req, res) => {
-    try {
-        await Todo.find({userId: '1'})
-            .then((todos) => {
-                res.status(200).json(todos);
-            })
-            .catch((err) => {
-                return res.status(404).json({status: err.name});
-            });
-    } catch (e) {
-        return res.status(404).json({status: e.name});
-    }
+router.get('/todo', (req, res) => {
+  Todo.find({userId: '1'})
+    .then((todos) => {
+      res.status(200).json(todos);
+    })
+    .catch((err) => {
+      return res.status(404).json({status: err.name});
+    });
 });
 
 router.post('/todo', async (req, res) => {
-    try {
-        let newTodo = await new Todo({
-            userId: '1',
-            title: req.body.title,
-            completed: false,
-            deadline: req.body.deadline,
-            priority: req.body.priority,
-        });
+  try {
+    let newTodo = await new Todo({
+      userId: '1',
+      title: req.body.title,
+      completed: false,
+      deadline: req.body.deadline,
+      priority: req.body.priority,
+    });
 
-        await newTodo.save();
+    await newTodo.save();
 
-        res.status(200).json({todo: newTodo, message: 'New task created'});
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: e.name})
-    }
+    res.status(200).json({todo: newTodo, message: 'New task created'});
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({message: e.name})
+  }
 
 });
 
 router.put('/todo/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const {title, deadline, priority} = req.body;
+  try {
+    const {id} = req.params;
+    const {title, deadline, priority} = req.body;
 
-        await Todo.findByIdAndUpdate(id,
-            {
-                title: title,
-                deadline: deadline,
-                priority: priority
-            },
-            {new: true, useFindAndModify: false},
-            function (err, result) {
-                if (err) {
-                    console.log(err);
-                    res.status(404).json({status: err.name});
-                } else {
-                    res.status(200).json({todo: result, message: 'Edit is successful'});
-                }
-            });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: e.name})
-    }
+    await Todo.findByIdAndUpdate(id,
+      {
+        title: title,
+        deadline: deadline,
+        priority: priority
+      },
+      {new: true, useFindAndModify: false},
+      function (err, result) {
+        if (err) {
+          console.log(err);
+          res.status(404).json({status: err.name});
+        } else {
+          res.status(200).json({todo: result, message: 'Edit is successful'});
+        }
+      });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({message: e.name})
+  }
 });
 
 router.put('/todo/:id/complete', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const {completed} = req.body;
+  try {
+    const {id} = req.params;
+    const {completed} = req.body;
 
-        await Todo.findByIdAndUpdate(id,
-            {
-                completed: completed
-            },
-            {new: true, useFindAndModify: false},
-            function (err, result) {
-                if (err) {
-                    res.status(404).json({status: err.name});
-                } else {
-                    res.status(200).json({todo: result, message: 'Edit is successful'});
-                }
-            });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: e.name})
-    }
+    await Todo.findByIdAndUpdate(id,
+      {
+        completed: completed
+      },
+      {new: true, useFindAndModify: false},
+      function (err, result) {
+        if (err) {
+          res.status(404).json({status: err.name});
+        } else {
+          res.status(200).json({todo: result, message: 'Edit is successful'});
+        }
+      });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({message: e.name})
+  }
 });
 
 router.delete('/todo/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
+  try {
+    const {id} = req.params;
 
-        await Todo.findByIdAndDelete(id,
-            function (err, result) {
-                if (err) {
-                    console.log(err);
-                    res.status(404).json({status: err.name});
-                } else {
-                    res.status(200).json({message: 'Task deleted'});
-                }
-            });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({message: e.name})
-    }
+    await Todo.findByIdAndDelete(id,
+      function (err, result) {
+        if (err) {
+          console.log(err);
+          res.status(404).json({status: err.name});
+        } else {
+          res.status(200).json({message: 'Task deleted'});
+        }
+      });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({message: e.name})
+  }
 });
 
 
